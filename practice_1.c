e1bd5096656648c0514428e89296f5bb25585f4f
Aisha Magomedova <magomedov.aisha@yandex.ru>
2024-09-30 00:51:29 +0400

    Add files via upload

---
+++ practice_1.c
@@ -0,0 +1,156 @@
+practice_1()
+{
+
+	web_add_auto_header("Priority", 
+		"u=0, i");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_set_sockets_option("SSL_VERSION", "2&3");
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	web_add_auto_header("Priority", 
+		"u=4");
+
+	web_submit_data("login.pl",
+		"Action=http://localhost:1080/cgi-bin/login.pl",
+		"Method=POST",
+		"TargetFrame=body",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home",
+		"Snapshot=t2.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=userSession", "Value={usersession}", ENDITEM,
+		"Name=username", "Value=jojo", ENDITEM,
+		"Name=password", "Value=bean", ENDITEM,
+		"Name=login.x", "Value=0", ENDITEM,
+		"Name=login.y", "Value=0", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		LAST);
+
+	web_url("Search Flights Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=search", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_auto_header("Origin", 
+		"http://localhost:1080");
+
+	lr_think_time(5);
+
+	web_submit_data("reservations.pl",
+		"Action=http://localhost:1080/cgi-bin/reservations.pl",
+		"Method=POST",
+		"TargetFrame=",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl?page=welcome",
+		"Snapshot=t4.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=advanceDiscount", "Value=0", ENDITEM,
+		"Name=depart", "Value=Frankfurt", ENDITEM,
+		"Name=departDate", "Value=09/30/2024", ENDITEM,
+		"Name=arrive", "Value=San Francisco", ENDITEM,
+		"Name=returnDate", "Value=10/01/2024", ENDITEM,
+		"Name=numPassengers", "Value=1", ENDITEM,
+		"Name=seatPref", "Value=None", ENDITEM,
+		"Name=seatType", "Value=Coach", ENDITEM,
+		"Name=findFlights.x", "Value=61", ENDITEM,
+		"Name=findFlights.y", "Value=14", ENDITEM,
+		"Name=.cgifields", "Value=roundtrip", ENDITEM,
+		"Name=.cgifields", "Value=seatType", ENDITEM,
+		"Name=.cgifields", "Value=seatPref", ENDITEM,
+		LAST);
+
+	web_submit_data("reservations.pl_2", 
+		"Action=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Snapshot=t5.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=outboundFlight", "Value=160;431;09/30/2024", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=seatType", "Value=Coach", ENDITEM, 
+		"Name=seatPref", "Value=None", ENDITEM, 
+		"Name=reserveFlights.x", "Value=22", ENDITEM, 
+		"Name=reserveFlights.y", "Value=8", ENDITEM, 
+		LAST);
+
+	web_revert_auto_header("Origin");
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	lr_think_time(16);
+
+	web_submit_data("reservations.pl_3", 
+		"Action=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Snapshot=t6.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=firstName", "Value=Jojo", ENDITEM, 
+		"Name=lastName", "Value=Bean", ENDITEM, 
+		"Name=address1", "Value=Street", ENDITEM, 
+		"Name=address2", "Value=City", ENDITEM, 
+		"Name=pass1", "Value=Jojo Bean", ENDITEM, 
+		"Name=creditCard", "Value=1234567890", ENDITEM, 
+		"Name=expDate", "Value=12", ENDITEM, 
+		"Name=oldCCOption", "Value=", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=seatType", "Value=Coach", ENDITEM, 
+		"Name=seatPref", "Value=None", ENDITEM, 
+		"Name=outboundFlight", "Value=160;431;09/30/2024", ENDITEM, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=returnFlight", "Value=", ENDITEM, 
+		"Name=JSFormSubmit", "Value=off", ENDITEM, 
+		"Name=buyFlights.x", "Value=21", ENDITEM, 
+		"Name=buyFlights.y", "Value=11", ENDITEM, 
+		"Name=.cgifields", "Value=saveCC", ENDITEM, 
+		LAST);
+
+	web_add_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	lr_think_time(5);
+
+	web_url("SignOff Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=flights", 
+		"Snapshot=t7.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	return 0;
+}
---
+++ practice_2.c
@@ -0,0 +1,156 @@
+practice_2()
+{
+
+	web_add_auto_header("Priority", 
+		"u=0, i");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_set_sockets_option("SSL_VERSION", "2&3");
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	web_add_auto_header("Priority", 
+		"u=4");
+
+	web_submit_data("login.pl",
+		"Action=http://localhost:1080/cgi-bin/login.pl",
+		"Method=POST",
+		"TargetFrame=body",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home",
+		"Snapshot=t2.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=userSession", "Value={usersession}", ENDITEM,
+		"Name=username", "Value={login}", ENDITEM,
+		"Name=password", "Value={password}", ENDITEM,
+		"Name=login.x", "Value=0", ENDITEM,
+		"Name=login.y", "Value=0", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		LAST);
+
+	web_url("Search Flights Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=search", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_auto_header("Origin", 
+		"http://localhost:1080");
+
+	lr_think_time(5);
+
+	web_submit_data("reservations.pl",
+		"Action=http://localhost:1080/cgi-bin/reservations.pl",
+		"Method=POST",
+		"TargetFrame=",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl?page=welcome",
+		"Snapshot=t4.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=advanceDiscount", "Value=0", ENDITEM,
+		"Name=depart", "Value={depart}", ENDITEM,
+		"Name=departDate", "Value={departDate}", ENDITEM,
+		"Name=arrive", "Value={arrive}", ENDITEM,
+		"Name=returnDate", "Value={returnDate}", ENDITEM,
+		"Name=numPassengers", "Value=1", ENDITEM,
+		"Name=seatPref", "Value={seatPref}", ENDITEM,
+		"Name=seatType", "Value={seatType}", ENDITEM,
+		"Name=findFlights.x", "Value=61", ENDITEM,
+		"Name=findFlights.y", "Value=14", ENDITEM,
+		"Name=.cgifields", "Value=roundtrip", ENDITEM,
+		"Name=.cgifields", "Value=seatType", ENDITEM,
+		"Name=.cgifields", "Value=seatPref", ENDITEM,
+		LAST);
+
+	web_submit_data("reservations.pl_2",
+		"Action=http://localhost:1080/cgi-bin/reservations.pl",
+		"Method=POST",
+		"TargetFrame=",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl",
+		"Snapshot=t5.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=outboundFlight", "Value=160;431;{departDate}", ENDITEM,
+		"Name=numPassengers", "Value=1", ENDITEM,
+		"Name=advanceDiscount", "Value=0", ENDITEM,
+		"Name=seatType", "Value={seatType}", ENDITEM,
+		"Name=seatPref", "Value={seatPref}", ENDITEM,
+		"Name=reserveFlights.x", "Value=22", ENDITEM,
+		"Name=reserveFlights.y", "Value=8", ENDITEM,
+		LAST);
+
+	web_revert_auto_header("Origin");
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	lr_think_time(16);
+
+	web_submit_data("reservations.pl_3",
+		"Action=http://localhost:1080/cgi-bin/reservations.pl",
+		"Method=POST",
+		"TargetFrame=",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl",
+		"Snapshot=t6.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=firstName", "Value={firstname}", ENDITEM,
+		"Name=lastName", "Value={lastname}", ENDITEM,
+		"Name=address1", "Value=Street", ENDITEM,
+		"Name=address2", "Value=City", ENDITEM,
+		"Name=pass1", "Value={firstname} {lastname}", ENDITEM,
+		"Name=creditCard", "Value={creditCard}", ENDITEM,
+		"Name=expDate", "Value=12", ENDITEM,
+		"Name=oldCCOption", "Value=", ENDITEM,
+		"Name=numPassengers", "Value=1", ENDITEM,
+		"Name=seatType", "Value={seatType}", ENDITEM,
+		"Name=seatPref", "Value={seatPref}", ENDITEM,
+		"Name=outboundFlight", "Value=160;431;{{departDate}}", ENDITEM,
+		"Name=advanceDiscount", "Value=0", ENDITEM,
+		"Name=returnFlight", "Value=", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		"Name=buyFlights.x", "Value=21", ENDITEM,
+		"Name=buyFlights.y", "Value=11", ENDITEM,
+		"Name=.cgifields", "Value=saveCC", ENDITEM,
+		LAST);
+
+	web_add_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	lr_think_time(5);
+
+	web_url("SignOff Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=flights", 
+		"Snapshot=t7.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	return 0;
+}
